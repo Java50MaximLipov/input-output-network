8e142aa55d72ed96fedbec057d2c04275097d721
mlipov <maxim.lipov@gmail.com>
2023-09-21 20:12:07 +0300

    HW-32 Solution

---
+++ src/telran/io/FilesCopy.java
@@ -0,0 +1,8 @@
+package telran.io;
+
+public class FilesCopy implements CopyFile {
+	@Override
+	public void copy(String pathToSource, String pathToDestination) throws Exception {
+		// TODO Auto-generated method stub
+	}
+}
