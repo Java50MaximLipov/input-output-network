1fc45565f10f6ecdd10b3df2c08597e014da0b37
mlipov <maxim.lipov@gmail.com>
2023-11-17 09:29:16 +0200

    231117 HW-45 adds read / write locks for synchronization

--- src/telran/employees/dto/SalaryDistribution.java
+++ src/telran/employees/dto/SalaryDistribution.java
@@ -3,4 +3,5 @@ package telran.employees.dto;
 import java.io.Serializable;
 
 public record SalaryDistribution(int min, int max, int amount) implements Serializable {
+
 }
---
+++ src/telran/net/TcpServer.java
@@ -0,0 +1,31 @@
+package telran.net;
+
+import java.net.*;
+
+public class TcpServer implements Runnable {
+	private int port;
+	private ApplProtocol protocol;
+	private ServerSocket serverSocket;
+
+	public TcpServer(int port, ApplProtocol protocol) throws Exception {
+		this.port = port;
+		this.protocol = protocol;
+		serverSocket = new ServerSocket(port);
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Server is listening on port " + port);
+		try {
+			while (true) {
+				Socket socket = serverSocket.accept();
+				ClientSessionHandler client = new ClientSessionHandler(socket, protocol);
+				Thread thread = new Thread(client);
+				thread.start();
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e.toString());
+		}
+	}
+
+}
